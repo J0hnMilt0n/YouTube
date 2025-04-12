df64f20a4f748dff50fccb2c81f0e839be4d5d65
J0hnMilt0n <mjohnmilton41@outlook.com>
2025-04-12 08:19:34 +0530

    fix logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -80,7 +80,7 @@ export default function Navbar() {
           </span>
         </div>
         <img
-          src="https://yt3.ggpht.com/wgnEbc2Ec2JYkeyzUbiHzDlAFObI2Btwo2YRCEF1aCMBiRc5E_zWy8-URBQS3EMQ1yzzaGFR=s88-c-k-c0x00ffffff-no-rj-mo"
+          src="ytlogo.png"
           className="w-9 h-9 rounded-full"
           alt="logo"
         />
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -99,27 +99,8 @@ export default function Sidebar() {
     },
   ];
 
-  const textLinks = [
-    [
-      "About",
-      "Press",
-      "Copyright",
-      "Contact us",
-      "Creator",
-      "Advertise",
-      "Developers",
-    ],
-    [
-      "Terms",
-      "Privacy",
-      "Policy & Safety",
-      "How YouTube works",
-      "Test new features",
-    ],
-  ];
-
   return (
-    <div className="w-2/12 bg-[#212121] pr-5 overflow-auto pb-8 sidebar">
+    <div className="mt-6 w-2/12 bg-[#212121] pr-5 overflow-auto pb-8 sidebar">
       <ul className="flex flex-col border-b-2 border-gray-700">
         {mainLinks.map(({ icon, name }) => {
           return (
@@ -173,21 +154,7 @@ export default function Sidebar() {
           );
         })}
       </ul>
-      <ul className="flex gap-2 flex-wrap text-sm p-4 text-zinc-400">
-        {textLinks[0].map((name) => {
-          return <li key={name}>{name}</li>;
-        })}
-      </ul>
-      <ul className="flex gap-2 flex-wrap text-sm p-4 text-zinc-400">
-        {textLinks[1].map((name) => {
-          return <li key={name}>{name}</li>;
-        })}
-      </ul>
-      <span className="px-4 text-sm text-zinc-400">&copy; 2022 Google</span>
-      <br />
-      <p className="px-4 pt-3 text-sm text-zinc-400">
-        This clone is for educational purpose only.
-      </p>
+      
     </div>
   );
 }
